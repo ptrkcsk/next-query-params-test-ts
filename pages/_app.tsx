eed406132c3d53ffd48d4b0440839ed027d90061
Patrik Csak <p@trikcsak.com>
2021-10-24 23:20:37 -0700

    Add next-query-params

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {NextQueryParamProvider} from 'next-query-params';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextQueryParamProvider>
+      <Component {...pageProps} />
+    </NextQueryParamProvider>
+  )
 }
 export default MyApp
